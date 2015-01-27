06065cae6f194b19448f37fe06a0d94e2e2482dd
Byrdann Fox <mobileupstate@gmail.com>
2015-01-26 19:40:51 -0500

    Project init...

---
+++ AACcess/AACcess/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AACcess
+//
+//  Created by Byrdann Fox on 1/26/15.
+//  Copyright (c) 2015 ExcepApps, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
